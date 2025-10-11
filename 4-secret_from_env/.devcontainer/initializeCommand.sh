4c6f2e089a2cdfbddcd92dfe77e3ec95f4ff33dd
Atsushi Morimoto (74th) <74th.tech@gmail.com>
2025-10-11 14:53:31 +0900

    fix

--- 4-secret_from_env/.devcontainer/initializeCommand.sh
+++ 4-secret_from_env/.devcontainer/initializeCommand.sh
@@ -1,4 +1,4 @@
 
 #!/bin/bash
 # .devcontainer/devcontainer.env にシークレットを環境変数で出力
-uv run .devcontainer/generate_bedrock_shortterm_api_key_to_stdout.py
+uv run .devcontainer/generate_bedrock_shortterm_api_key_to_envfile.py

c2cf113c1e9aaa46ea87a960444714275167695e
Atsushi Morimoto (74th) <74th.tech@gmail.com>
2025-10-26 15:59:40 +0900

    UID変更はきくのか？

---
+++ 6-golang_with_language_official_image/.devcontainer/initializeCommand.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+# マウント対象のディレクトリを予め作成しておく
+# Dockerランタイムによっては存在しないディレクトリをマウントできなかったり、
+# root権限で作成される場合があるため
+mkdir -p ~/.cache/devcontainer/go-cache-go-build
+mkdir -p ~/.cache/devcontainer/go-gomod

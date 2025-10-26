ab5cebb6bf0e4647730113ae387cbf0ee955d6b9
Atsushi Morimoto (74th) <74th.tech@gmail.com>
2025-10-26 17:43:19 +0900

    golangを極める

--- 6-golang_with_devcontainer_official_image/.devcontainer/postCreateCommand.sh
+++ 6-golang_with_devcontainer_official_image/.devcontainer/postCreateCommand.sh
@@ -2,5 +2,9 @@
 # sudo権限の剥奪
 sudo rm -rf /etc/sudoers.d/*
 
+# golangci-lintのバージョン固定
+GOLANGCI_LINT_VERSION=v1.64.8
+curl -sSfL https://raw.githubusercontent.com/golangci/golangci-lint/HEAD/install.sh | sh -s -- -b $(go env GOPATH)/bin $GOLANGCI_LINT_VERSION
+
 # 依存パッケージのインストール
 go get ./...
--- 6-golang_with_language_official_image/.devcontainer/postCreateCommand.sh
+++ 6-golang_with_language_official_image/.devcontainer/postCreateCommand.sh
@@ -1,4 +1,15 @@
 #!/bin/bash
 
+# Goの各ツールのインストール
+# ユーザ権限でインストールするので、ユーザが更新できる
+go install golang.org/x/tools/gopls@latest
+go install github.com/cweill/gotests@latest
+go install github.com/josharian/impl@latest
+go install github.com/go-delve/delve/cmd/dlv@latest
+
+# golangci-lintのバージョン固定
+GOLANGCI_LINT_VERSION=v1.64.8
+curl -sSfL https://raw.githubusercontent.com/golangci/golangci-lint/HEAD/install.sh | sh -s -- -b $(go env GOPATH)/bin $GOLANGCI_LINT_VERSION
+
 # 依存パッケージのインストール
 go get ./...
